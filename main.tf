98429728ddbf0b5ec8a5a96d4b9c593995404752
Ryan Hartkopf <ryan.hartkopf@propellerhealth.com>
2019-05-30 12:19:18 -0500

    Complete upgrade to-dos

--- main.tf
+++ main.tf
@@ -31,14 +31,6 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     ]
 
     effect = "Allow"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibilty in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [local.sns_topic_arn]
 
     principals {
@@ -59,14 +51,6 @@ data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
     sid     = "Allow ${local.alert_for} CloudwatchEvents"
     actions = ["sns:Publish"]
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibilty in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [local.sns_topic_arn]
 
     principals {
